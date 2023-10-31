3e49ed5c72d2d6af8c4305029a6978a74de80b21
Bati Myles <batim10@gmail.com>
2023-10-31 12:28:01 -0500

    Function to calculate Spearman or Pearson correlation coefficient with p-values
    If relationship not linear uses another function to attemp box cox transformation

---
+++ R/Correlation.R
@@ -0,0 +1,29 @@
+#Calculates Pearson's Coefficient
+
+Correlation <- function(x,y){
+  dtaVect <- xyVectors(table)
+  x <- dtaVect$x
+  y <- dtaVect$y
+
+  response <- readline(
+  prompt = "Do your variables appear to have a linear relationship? Answer Y or N:"
+  )
+  if (response == Y){
+    response2 <- readline(
+      prompt = "Are your variables normally distributed? Answer Y or N")
+    if (response2 == Y){
+      relationship <- cor.test(x,y,method = "pearson")
+    }else if (response32 == N){
+      relationship <- cor.test(x,y,method = "spearman")
+    } else {
+      stop("Invalid input")
+    }
+  } else if (response == N){
+    output <- transformation(x,y)
+    if (is.NA(output$x)){
+      relationship <- cor.test(output$x, output$y)
+    } else {
+      stop("Your Data is Not Appropriate for Correlation Testing")
+    }
+  } print(relationship)
+}
