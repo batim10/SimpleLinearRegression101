84d8e8150d1ef35a68c0e19f4083322a5449bca9
Bati Myles <batim10@gmail.com>
2023-11-11 12:41:37 -0600

    Function to test if data is normally distributed

---
+++ R/dataVectorsTitles.R
@@ -0,0 +1,19 @@
+#More tools to help assess if data is normally distributed
+
+xyVectors <- function(table){
+  response <- readline(prompt = "Is your response variable in column 1 or 2?")
+  if (response == 1){
+    y <- table[,1]
+    x <- table[,2]
+    x_lab <- colnames(table)[2]#use header title for x labels
+    y_lab <- colnames(table)[1]#use header title for y labels
+  } else if (response == 2){
+    y <- table[,2]
+    x <- table[,1]
+    x_lab <- colnames(table)[1]
+    y_lab <- colnames(table)[2]
+  } else {
+    stop("Invalid response")
+  }
+  return(list(x,y, x_lab, y_lab))
+}
