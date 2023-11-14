e1d9f5aaa39d6153c3c24b4660a9ebc1dc04ffb6
Bati Myles <batim10@gmail.com>
2023-11-14 13:24:59 -0600

    Added comments
    Removed some code to put in a different function

--- R/outputLM.R
+++ R/outputLM.R
@@ -6,71 +6,17 @@ SimpleLinear <- function(x,y){
   x <- dtaVect$x
   y <- dtaVect$y
 
-  plot(x, y, )
+  plot(x, y, xlab = dtaVect$x_lab, ylab = dtaVect$y_lab)
 
   n <- length(x)
-  x <- cbind(rep(1,n),x)
+  x <- cbind(rep(1,n),x) #Add column of 1s to get intercept
 
-  fit <- lm(y~x)
-  summary(fit)
+  fit <- lm(y~x) #Create linear model
+  summary(fit) #Plot residuals
 
   #Output the coefficients and the p value indicating if this is a significant
   #Relationship
 
   par(mfrow = c(2,2))
   plot(fit)
-
-  print(" We will be assessing the validity of our model before performing
-        more analysis. A valid model must adhere to the assumptions of
-        the linear model.
-        These assumptions are:
-        * Residuals are random
-        * Residuals are independent
-        * Residuals are have a normal distribution
-        * Residials demonstrate homoscedasticity")
-
-  responses = rep(0,NA)
-
-  print("The residuals vs fitted plot should demonstrate a random scatter of
-        of residuals on both sides of 0 without a signficant change or trend
-        across fitted values indicating homoscedasticity and linearity")
-
-  responses[1] <- readline(
-    prompt <- "Do your residuals appear to be randomly scattered? Answer Y or N")
-
-  print("Your Normal Q-Q plol should show that your standardized residals fit
-        the predicted line well indicating that they are normally distributed")
-
-  responses[2] <- readline(
-    prompt<- "Do your residuals have a normal distribution? Answer Y or N"
-  )
-
-  print("Your Scale-Location plot should demonstrate a fitted line that is
-        flat. This demonstrates homoscedasticity ")
-
-  responses[3] <- readline(
-    prompt = "Do your residuals demonstrate homoscedasticity? Answer Y or N")
-
-  print("Your Residuals vs Leverage plot will indicate if you have outliers
-        that have high leverage and residuals whcih can have a disproportionate
-        affect on your model. These points will often have large Cook's distance")
-
-  responses[4] <- readline(
-    prompt = "Do you have points with high Cook's distance?"
-  )
-
-  if (all(responses == "T"){
-    print"Your model is valid"
   }
-
-  if (residualAnalysis()){
-
-  }else{
-
-  }
-
-  }
-
-  #Assessing validity of model: Errors with constant variance? Outliers/leverage points?/large sample size
-
-}
