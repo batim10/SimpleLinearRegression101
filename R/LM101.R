d5e03a387f818261c8ea5be169eaabd119f85300
Bati Myles <batim10@gmail.com>
2023-11-19 01:23:38 -0600

    Cleaned up some comments; only doing confidence intervals instead of confidence and prediction
    Added bootstrapping to calculate coefficients and confidence intervals

--- R/LM101.R
+++ R/LM101.R
@@ -21,17 +21,47 @@ SLR101 = function(){
   step7 <- modelCheck()
 
   print("If your model is valid, we will use your model to find Confidence Intervals for mean respose and Predictor Intervals
-        for an individual response variable given a predictor variable value")
-  userResponse <- readline(prompt <- "At which value of your predictor variable would you like to calculate Confidence and Prediction Intervals?")
+        for an individual response variable given a predictor variable valueat a level of .95")
+  userResponse <- readline(prompt <- "At which value of your predictor variable would you like to calculate Confidence Intervals?")
 
   if (step7){
-    CI <- predict(step6, newdata=data.frame(x_lab = userResponse), interval = "confidence")
-    PI <- predict(step6, newdata=data.frame(x_lab = userResponse), interval = "prediction")
-  }else if (step7 = "Boot"){
-    bootStrap(x,y)
-    CI
-    PI
+    CI <- predict(step6, newdata=data.frame(x_lab = userResponse), interval = "confidence", level = .95)
+    cat("The Confidence Interval for your response variable at your given predictor variable value
+        is",CI,"\n")
+    }else if (step7 = "Boot"){
+
+    print("As you require bootstrapping to calculate Confidence intervals for your
+          response variable, we will also use bootstrapping to calculate your coefficients")
+
+    B <- 1000
+
+    yhat <- function(x,y){
+      boot_indices <- sample(1:nrow(x), replace = TRUE)
+
+      boot_model <- lm(y[boot_indices]~x[boot_indices])
+
+      boot_coef <- coef(boot_model)
+
+      s <- sum(boot_coef*c(1,userResponse))
+
+      return(list (s = s,boot_coef = boot_coef))
+
+    }
+
+    #Apply function yhat to data in table 1000 times using boot library/function
+    int_boot <- boot::boot(table,yhat,B)
+
+    #Get confidence intervals with boot.ci function from boot librart
+    CI_yhat <- boot::boot.ci(int_boot$s, type = "perc") #Get confidence intervals
+
+    #Use mean betas as estimates for model
+    mean_betas <- colMeans(int_boot$boot_coef)
+
+
+  cat("The Confidence Interval for your response variable given your predictor variable is",CI_yhat)
+
+  cat("Your estimated model is: Y =",mean_betas[1],"+",mean_betas[2],"X")
   }else{
-    stop("Your data cannot be modeled using simple linear regression at this time")
+    stop("Your data as it is cannot be modeled using simple linear regression")
   }
 }
