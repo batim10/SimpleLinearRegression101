331bf0b5abe88a8ba3ebdfaded7b6f4baacdaa3d
Bati Myles <batim10@gmail.com>
2023-11-21 17:40:28 -0600

    Fixed syntax error and used tolower to allow user to input upper or lower case letters
    Restructured prompts with a loop to take user back to the top is invalid input used

--- R/Correlation.R
+++ R/Correlation.R
@@ -1,32 +1,53 @@
 #Calculates Pearson's Coefficient
 
 Correlation <- function(x,y){
-  dtaVect <- xyVectors(table)
-  x <- dtaVect$x
-  y <- dtaVect$y
 
+  while(TRUE){
   response <- readline(
-
-    prompt <- "Do your variables appear to have a linear relationship? Answer Y or N:"
+  prompt <- "Do your variables appear to have a linear relationship? Answer Y or N:"
   )
-  if (response == Y){
-    response2 <- readline(
-      prompt = "Are your variables normally distributed? Answer Y or N")
-    if (response2 == Y){
-      relationship <- cor.test(x,y,method = "pearson")
-    }else if (response32 == N){
-      relationship <- cor.test(x,y,method = "spearman")
-    } else {
-      stop("Invalid input")
+  if (tolower(response) %in% c("y","n")){
+
+    if (tolower(response) == "y"){
+      response2 <- readline(
+        prompt = "Are your variables normally distributed? Answer Y or N")
+
+       if (tolower(response2) %in% c("y","n")){
+        if (tolower(response2) == "y"){
+          relationship <- cor.test(x,y,method = "pearson")
+        }else{
+        relationship <- cor.test(x,y,method = "spearman")
+        break
+        }
+      }else{
+        cat("Invalid input")
+        }
+    }else{
+      response3 <- readline(
+        prompt = "Are your variables normally distributed? Answer Y or N")
+
+      if (tolower(response3) %in% c("y","n")){
+        if (tolower(response3) == "y"){
+          stop("A correlation coefficient cannot be calculated for your data")
+        }else{
+          output <- transformation(x,y)
+          if (is.NA(output$x) == FALSE){
+            relationship <- cor.test(output$x, output$y)
+            break
+          }else{
+            stop("Your Data is Not Appropriate for Correlation Testing")
+          }
+        }
+     }else{
+        cat("Invalid Input")
+     }
     }
-  } else if (response == N){
-    output <- transformation(x,y)
-    if (is.NA(output$x)){
-      relationship <- cor.test(output$x, output$y)
-    } else {
-      stop("Your Data is Not Appropriate for Correlation Testing")
-    }
+
+    }else{
+    cat("Invalid Input")
   }
+
+}
     print(relationship)
     print("The strength and direction of correlation are given by rho or cor.
             If your p-value is less than your significance level then this
