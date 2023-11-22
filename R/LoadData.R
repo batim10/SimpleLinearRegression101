6c505e76c06ebece7c13993341dc110fea4a143a
Bati Myles <batim10@gmail.com>
2023-11-21 23:16:59 -0600

    syntax fixes

--- R/LoadData.R
+++ R/LoadData.R
@@ -17,24 +17,24 @@ Load = function(loc){
   #Need to query about using a decimal point for appropriate formatting
   response1 = readline(prompt = "Do you use . as a decimal point? Response Y or N")
 
-  if (response1 != "Y" & response1 != "N"){
+  if (tolower(response1) != "y" & tolower(response1) != "n"){
     stop("Invalid Response")
   }
 
   #use lower case of user input to read file extension
   if (tolower(suffix) == "csv"){
-    if (response1 == "Y"){
+    if (tolower(response1) == "y"){
     dta = read.csv(loc)
-    }else if (response1 == "N"){
+    }else if (tolower(response1) == "n"){
       dta = read.csv2(loc)}
   }else if (tolower(suffix) == "txt"){
     response2 = readline(prompt = "Is your data in a table or tab delimited? Respond table or tab: ")
     if (tolower(response2) == "table"){
       dta = read.table(loc, header = TRUE)
   }else if (tolower(response2) == "tab"){
-      if (response1 == "Y"){
+      if (tolower(response1) == "y"){
         dta = read.delim(loc)
-      }else if (response1 == "N"){
+      }else if (tolower(response1) == "n"){
       dta = read.delim2(loc)}
       }else if(tolower(suffix) == "xlsx"){
       readx1::read.excel(loc)
