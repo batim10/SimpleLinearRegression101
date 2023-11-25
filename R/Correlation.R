eec42eb57abf53530bdf0995e412bef2e4aec16c
Bati Myles <batim10@gmail.com>
2023-11-25 11:33:06 -0600

    Added some spaces to make prompts easier to read
    Fixed some logic errors with my while loops
    
    Using new conditional for code to execute based on transfomation output
    
    Changed print to cat for better visual output

--- R/Correlation.R
+++ R/Correlation.R
@@ -2,60 +2,75 @@
 #Pearson's if linear and normal
 #Spearman's if linear and not normal
 
-Correlation <- function(x,y){
+Correlation <- function(x,y, x_lab,y_lab){
 
   #Use while loop to reprompt user if they use invalid input
   while(TRUE){
 
-    response <- readline(
-  prompt <- "Do your variables appear to have a linear relationship? Answer Y or N:"
+  response <- readline(
+  prompt <- "Do your variables appear to have a linear relationship? Answer Y or N: "
   )
-  #checking for valid input
-  if (tolower(response) %in% c("y","n")){
 
-    if (tolower(response) == "y"){
+  if (tolower(response) %in% c("y","n")){
+    break
+  } else {
+    cat("Invalid input")
+  }
+  }
+  #checking for valid input
+  if (tolower(response) == "y"){
+    while(TRUE){
+
       response2 <- readline(
-        prompt = "Are your variables normally distributed? Answer Y or N")
+        prompt = "Are your variables normally distributed? Answer Y or N: ")
 
       #checking for valid input to obtain correlation coefficients
        if (tolower(response2) %in% c("y","n")){
-        if (tolower(response2) == "y"){
-          relationship <- cor.test(x,y,method = "pearson")
-        }else{
-        relationship <- cor.test(x,y,method = "spearman")
-        break #Break from loop if valid input
-        }
+         break
+       }else {
+         cat("Invalid Input")}
+    }
+      if (tolower(response2) == "y"){
+        relationship <- cor.test(x,y,method = "pearson")
       }else{
-        cat("Invalid input")
+      relationship <- cor.test(x,y,method = "spearman")
         }
+
     }else{
+
+      while(TRUE){
       response3 <- readline(
-        prompt = "Are your variables normally distributed? Answer Y or N")
+        prompt = "Are your variables normally distributed? Answer Y or N: ")
 
       if (tolower(response3) %in% c("y","n")){
+        break
+      }else{
+        cat("Invalid input")
+      }
+      }
+
         if (tolower(response3) == "y"){
           stop("A correlation coefficient cannot be calculated for your data")
         }else{
-          output <- transformation(x,y)
-          if (is.NA(output$x) == FALSE){
+          output <- transformation(x,y,x_lab,y_lab)
+          if (length(output$x) > 1){
             relationship <- cor.test(output$x, output$y, method = "pearson")
-            break
           }else{
             stop("Your Data is Not Appropriate for Correlation Testing")
           }
         }
-     }else{
-        cat("Invalid Input")
-     }
-    }
 
-    }else{
-    cat("Invalid Input")
-  }
 
 }
-    print(relationship)
-    print("The strength and direction of correlation are given by rho or cor.
-            If your p-value is less than your significance level then this
-            correlation is statistically significant.")
+
+    cat("
+           * The strength and direction of correlation are given by rho or cor.
+
+           * The closer rho or cor are to 1 the stronger the correlation
+
+           *The sign of rho or cor indicate the direction of the correlation
+
+           * If your p-value is less than your significance level then this
+            correlation is statistically significant.\n")
+    return(relationship)
 }
