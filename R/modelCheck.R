776c6bbf7229a3ffb5b0b57c8b8da9eb97163829
Bati Myles <batim10@gmail.com>
2023-11-26 17:07:23 -0600

    Fixed syntax errors
    Fixed output of function to use in next step of primary function

--- R/modelCheck.R
+++ R/modelCheck.R
@@ -1,6 +1,10 @@
 #Check model validity
+#Transform data if needed
 
-validity <- function(){
+validity <- function(x,y,x_lab,y_lab){
+
+while(TRUE){
+
 cat(" We will be assessing the validity of our model before performing
         more analysis. A valid model must adhere to the assumptions of
         the linear model.
@@ -19,65 +23,80 @@ cat("The residuals vs fitted plot should demonstrate a random scatter of
 
 
 responses[1] <- readline(
-  prompt <- "Do your residuals appear to be randomly scattered? Answer Y or N")
+  prompt <- "Do your residuals appear to be randomly scattered? Answer Y or N: ")
 
-cat("Your Normal Q-Q plot should show that your standardized residals fit
+cat("
+        Your Normal Q-Q plot should show that your standardized residals fit
         the predicted line well indicating that they are normally distributed")
 
 responses[2] <- readline(
   prompt<- "Do your residuals have a normal distribution? Answer Y or N
 
   Note: If your dataset is large you can answer Y even residials do not
-  demonstrate a normal distribution."
+  demonstrate a normal distribution.: "
 )
 
-cat("Your Scale-Location plot should demonstrate a fitted line that is
+cat("
+        Your Scale-Location plot should demonstrate a fitted line that is
         flat. This demonstrates homoscedasticity ")
 
 responses[3] <- readline(
-  prompt = "Do your residuals demonstrate homoscedasticity? Answer Y or N")
+  prompt = "Do your residuals demonstrate homoscedasticity? Answer Y or N: ")
 
-cat("Your Residuals vs Leverage plot will indicate if you have outliers
+cat("
+        Your Residuals vs Leverage plot will indicate if you have outliers
         that have high leverage which can have a disproportionate
         affect on your model. These points will often have large Cook's distance")
 
 responses[4] <- readline(
   prompt = "Do all data point have Cook's distance smaller than the limit (dotted line)?
-  Answer Y or N"
+  Answer Y or N: "
 )
 
 #check for appropriate user input
-if (any(responses != "Y") & any(responses != "N")){
+if (any(tolower(responses) != "y") & any(tolower(responses) != "n")){
   stop("Invalid Response")
 
-  } else if (all(responses == "Y")){ #Valid model if all assumptions met
+  } else if (all(tolower(responses) == "y")){ #Valid model if all assumptions met
   cat("Your model is valid \n")
-  return (TRUE)
+  return (list(x = x, y = y))
+    break
 
-  }else if (any(responses) == "N"){
-  print("Your model fails to validate one or more assumptions of simple linear regression.")
-  response1 <- readline(
-    prompt = "Have you already transformed your data to normality? Respond Y or N"
+  }else if (any(tolower(responses) == "n")){
+  cat("Your model fails to validate one or more assumptions of simple linear regression.")
+
+   response1 <- readline(
+    prompt = "Have you already transformed your data to normality? Respond Y or N: "
   )
 
-  if (response1 == "N"){
-    print("Transform your data and submit transformed data to attempt to address this issue.")
-    return(FALSE)
+    if (tolower(response1) == "n"){
+      trans <- transformation(x,y,x_lab,y_lab)
+      x <- trans$x
+      y <- trans$y
+      SimpleLinear(x,y)
 
-    }else if (response1 == "Y"){
-  if (responses[4] == "N"){
-    print("You have outliers with high leverage. Examine these data points to
+    }else if (tolower(response1) == "y"){
+      if (tolower(responses[4]) == "n"){
+      cat(
+          "You have outliers with high leverage. Examine these data points to
           determine if they should remain in the model.
           * If they can be removed, resubmit your data without these points.
           * If they cannot be removed consider adding an indicator variable to
-           your model and resubmit")
-    return(FALSE)
-  }else{
-    if (responses[2] == "N" & all(responses[-2]) == "Y"){
-      print("You can use bootstrapping to obtain a linear model")
-      return("Boot")
+           your model and create a multivariable model")
+        return(NA)
+        break
+      }else if (tolower(responses[4]) == "y"){
+        if (tolower(responses[2]) == "n" & all(tolower(responses[-2])) == "y"){
+        cat("You can use bootstrapping to obtain a linear model")
+        return("Boot")
+          break
+        }else{
+          return(NA)
+          break
+        }
     }
   }
+ }
 }
 }
-}
+
