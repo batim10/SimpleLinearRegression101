20f20e3bbc629974120fede1ab8e27710858b1e2
Bati Myles <batim10@gmail.com>
2023-11-26 16:51:36 -0600

    Changed the source paths
    Fixed syntax

--- R/LM101.R
+++ R/LM101.R
@@ -5,19 +5,19 @@
 #path for my dataset is "C:\Users\batim\OneDrive\Documents\STAT608\TextbookResources\Data\Data\my_airlines.txt"
 #Name rnorm data, then y can be that times 2 plus 5
 
+source("C:/Users/batim/OneDrive/Documents/SimpleLinearRegression101/R/Correlation.R")
+source("C:/Users/batim/OneDrive/Documents/SimpleLinearRegression101/R/dataVectorsTitles.R")
+source("C:/Users/batim/OneDrive/Documents/SimpleLinearRegression101/R/dataVisualization.R")
+source("C:/Users/batim/OneDrive/Documents/SimpleLinearRegression101/R/Introduction.R")
+source("C:/Users/batim/OneDrive/Documents/SimpleLinearRegression101/R/LM101.R")
+source("C:/Users/batim/OneDrive/Documents/SimpleLinearRegression101/R/LoadData.R")
+source("C:/Users/batim/OneDrive/Documents/SimpleLinearRegression101/R/modelCheck.R")
+source("C:/Users/batim/OneDrive/Documents/SimpleLinearRegression101/R/outputLM.R")
+source("C:/Users/batim/OneDrive/Documents/SimpleLinearRegression101/R/Transformation.R")
+source("C:/Users/batim/OneDrive/Documents/SimpleLinearRegression101/R/y_pred.R")
+source("C:/Users/batim/OneDrive/Documents/SimpleLinearRegression101/R/boot_coefs.R")
 
 SLR101 = function(){
-
-  source("Correlation.R")
-  source("dataVectorsTitles.R")
-  source("dataVisualization.R")
-  source("Introduction.R")
-  source("LM101.R")
-  source("LoadData.R")
-  source("modelCheck.R")
-  source("outputLM.R")
-  source("Transformation.R")
-
 
   step1 <- intro()
 
@@ -32,56 +32,63 @@ SLR101 = function(){
 
   step4 <- Visual(x,y,x_lab,y_lab)
 
-  step5 <- Correlation(x,y)
+  step5 <- Correlation(x,y,x_lab,y_lab)
 
   step6 <- SimpleLinear(x,y)
 
-  step7 <- modelCheck()
+  step7 <- validity(x,y,x_lab,y_lab)
 
-  cat("If your model is valid, we will use your model to find Confidence Intervals for mean respose and Predictor Intervals
-        for an individual response variable given a predictor variable valueat a level of .95")
+  cat("
+        If your model is valid, we will use your model to find Confidence Intervals for mean respose and Predictor Intervals
+        for an individual response variable given a predictor variable value at a level of .95")
 
-  userResponse <- readline(prompt <- "At which value of your predictor variable would you like to calculate Confidence Intervals?")
+  userResponse <- readline(
+  prompt <- "Value of your predictor variable to calculate Confidence Intervals: ")
 
-  if (step7){
-    CI <- predict(step6, newdata=data.frame(x_lab = userResponse), interval = "confidence", level = .95)
+  #Check if step 7 output can be used for prediction
+  if (length(step7) > 1){
+
+    x <- step7$x
+    y <- step7$y
+
+    model <- lm(y~x)
+
+
+    #use linear model to create confidence interval at given prediction variable
+    CI <- predict(model, newdata = data.frame(x = as.numeric(userResponse)), interval = "confidence", level = .95)
 
     cat("The Confidence Interval for your response variable at your given predictor variable value
         is",CI,"\n")
     }else if (step7 == "Boot"){
 
-    cat("As you require bootstrapping to calculate Confidence intervals for your
+    cat("
+          As you require bootstrapping to calculate Confidence intervals for your
           response variable, we will also use bootstrapping to calculate your coefficients")
+
     #Number of bootstrap samples
     B <- 1000
 
+    boot_df <- data.frame(x_lab = x, y_lab= y)
+    colnames(boot_df) <- c(x_lab,y_lab)
+
+    #yhat function requires two arguments, often second argument are incides
+    indices <- sample(1:length(x), replace = TRUE) #Sample to bootstrap
+
     #Predicted value of Y based on linear model; will create B bootstraps of this value
-    yhat <- function(x,y){
-      boot_indices <- sample(1:nrow(x), replace = TRUE) #Sample to bootstrap
-
-      boot_model <- lm(y[boot_indices]~x[boot_indices])
-
-      boot_coef <- coef(boot_model)
-
-      s <- sum(boot_coef*c(1,userResponse))
-
-      return(list (s = s,boot_coef = boot_coef))
-
-    }
 
     #Apply function yhat to data in table 1000 times using boot library/function
-    int_boot <- boot::boot(table,yhat,B)
+    int_boot <- boot::boot(boot_df,yhat,B)
 
-    #Get confidence intervals with boot.ci function from boot librart
-    CI_yhat <- boot::boot.ci(int_boot$s, type = "perc") #Get confidence intervals
+    #Get confidence intervals with boot.ci function from boot library
+    CI_yhat <- boot::boot.ci(int_boot, type = "perc") #Get confidence intervals
 
     #Use mean betas as estimates for model
-    mean_betas <- colMeans(int_boot$boot_coef)
+    int_boot_coef <-  boot::boot(boot_df,boot_betas,B)
 
 
   cat("The Confidence Interval for your response variable given your predictor variable is",CI_yhat)
 
-  cat("Your estimated model is: Y =",mean_betas[1],"+",mean_betas[2],"X")
+  cat("Your estimated model coefficients are",int_boot_coef)
   }else{
     stop("Your data as it is cannot be modeled using simple linear regression")
   }
