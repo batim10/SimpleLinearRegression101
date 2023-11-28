44580c9d69d8baecdaedf4308bdf6fbcb96380b8
Bati Myles <batim10@gmail.com>
2023-11-28 14:14:09 -0600

    Adding new lines since cat doesn't do this automatically

--- R/modelCheck.R
+++ R/modelCheck.R
@@ -1,5 +1,6 @@
 #Check model validity
 #Transform data if needed
+#problem with loop
 
 validity <- function(x,y,x_lab,y_lab){
 
@@ -12,14 +13,15 @@ cat(" We will be assessing the validity of our model before performing
         * Residuals are random
         * Residuals are independent
         * Residuals are have a normal distribution
-        * Residials demonstrate homoscedasticity/constant variance")
+        * Residials demonstrate homoscedasticity/constant variance \n")
 
 responses = rep(NA,4) #Initialize empty vector
 
 
-cat("The residuals vs fitted plot should demonstrate a random scatter of
+cat("
+        The residuals vs fitted plot should demonstrate a random scatter of
         of residuals on both sides of 0 without a signficant change or trend
-        across fitted values indicating homoscedasticity and linearity")
+        across fitted values indicating homoscedasticity and linearity \n")
 
 
 responses[1] <- readline(
@@ -38,7 +40,7 @@ responses[2] <- readline(
 
 cat("
         Your Scale-Location plot should demonstrate a fitted line that is
-        flat. This demonstrates homoscedasticity ")
+        flat. This demonstrates homoscedasticity \n ")
 
 responses[3] <- readline(
   prompt = "Do your residuals demonstrate homoscedasticity? Answer Y or N: ")
@@ -46,7 +48,7 @@ responses[3] <- readline(
 cat("
         Your Residuals vs Leverage plot will indicate if you have outliers
         that have high leverage which can have a disproportionate
-        affect on your model. These points will often have large Cook's distance")
+        affect on your model. These points will often have large Cook's distance \n")
 
 responses[4] <- readline(
   prompt = "Do all data point have Cook's distance smaller than the limit (dotted line)?
@@ -54,16 +56,22 @@ responses[4] <- readline(
 )
 
 #check for appropriate user input
-if (any(tolower(responses) != "y") & any(tolower(responses) != "n")){
-  stop("Invalid Response")
+#Note: have to use tolower separately from %in% because in looks at entire vector
+#while tolower is applied elementwise
 
-  } else if (all(tolower(responses) == "y")){ #Valid model if all assumptions met
+
+#make a vector of all responses in lowercase
+responses <- tolower(responses)
+
+if (all(responses %in% c("y","n"))){
+
+  if (all(responses == "y")){ #Valid model if all assumptions met
   cat("Your model is valid \n")
   return (list(x = x, y = y))
     break
 
-  }else if (any(tolower(responses) == "n")){
-  cat("Your model fails to validate one or more assumptions of simple linear regression.")
+  }else if (any(responses == "n")){
+  cat("Your model fails to validate one or more assumptions of simple linear regression.\n")
 
    response1 <- readline(
     prompt = "Have you already transformed your data to normality? Respond Y or N: "
@@ -76,27 +84,30 @@ if (any(tolower(responses) != "y") & any(tolower(responses) != "n")){
       SimpleLinear(x,y)
 
     }else if (tolower(response1) == "y"){
-      if (tolower(responses[4]) == "n"){
-      cat(
+      if (responses[4] == "n"){
+      stop(
           "You have outliers with high leverage. Examine these data points to
           determine if they should remain in the model.
           * If they can be removed, resubmit your data without these points.
           * If they cannot be removed consider adding an indicator variable to
-           your model and create a multivariable model")
-        return(NA)
-        break
-      }else if (tolower(responses[4]) == "y"){
-        if (tolower(responses[2]) == "n" & all(tolower(responses[-2])) == "y"){
-        cat("You can use bootstrapping to obtain a linear model")
+           your model and create a multivariable model \n")
+
+      }else if (responses[4] == "y"){
+        if (responses[2] == "n" & all(responses[-2] == "y")){
+        cat("You can use bootstrapping to obtain a linear model \n")
         return("Boot")
           break
-        }else{
-          return(NA)
-          break
+      }else{
+          stop("Your data, as is, cannot be used to create a simple linear regression model \n")
         }
     }
+    }else{
+    cat("Invalid Response \n")
   }
  }
+}else{
+    cat("Invalid Response \n")
+  }
 }
 }
 
