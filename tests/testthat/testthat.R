c31043bbdb38780e357b5be654b2bf22d40be09b
Bati Myles <batim10@gmail.com>
2023-12-05 01:53:54 -0600

    Was abl to add one more test that kind of works; no errors but doesn't give a "pass" like the first expect_error test

--- tests/testthat/testthat.R
+++ tests/testthat/testthat.R
@@ -10,3 +10,22 @@ library(testthat)
 library(SimpleLinearRegression101)
 
 test_check("SimpleLinearRegression101")
+
+library(testthat)
+
+
+x <- rnorm(100)
+y <- 2*x + 5
+
+table <- cbind(x,y)
+
+testthat::test_that("Appropriate Errors while Loading Data", {
+  expect_error(Load(abcsde))
+  expect_error(Load(abcde.tva))
+})
+
+
+testthat::test_that("Appropriate data type output from Simple Linear", {
+  expect_s3_class(SimpleLinear(x,y), "summary.lm")
+})
+
